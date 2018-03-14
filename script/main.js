a2265d9fd07c4b3ef9a4c332d916278576139ab4
Darqoo <d.smolag@gmail.com>
2018-03-14 15:56:49 +0100

    fadeOut and fadeIn for logo on centerpage

--- script/main.js
+++ script/main.js
@@ -1,21 +1,22 @@
 window.addEventListener('load', () => {
   $("#loader").animate({
     opacity: 0
-  }, 1000,'linear', () => $("#loader").hide())
+  }, 1000, 'linear', () => $("#loader").hide())
 });
 
 (() => {
-    $(document).ready(() => {
-        $(".top_menu").hide();
-        $(() => {
-            $(window).scroll(() => {
-                if ($(this).scrollTop() > 200) {
-                    $('.top_menu').fadeIn();
-                } else {
-                    $('.top_menu').fadeOut();
-                }
-            });
-        });
-
+  $(document).ready(() => {
+    $(".top_menu").hide();
+    $(() => {
+      $(window).scroll(() => {
+        if ($(window).scrollTop() >= 200) {
+          $('.top_menu').fadeIn();
+          $('.home_page_logo').fadeOut();
+        } else if ($(window).scrollTop() <= 200) {
+          $('.top_menu').fadeOut();
+          $('.home_page_logo').fadeIn();
+        }
+      });
     });
+  });
 })();
