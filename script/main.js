841bdc9932d65d5a98a8ecabb37f6c2f1338f058
Darqoo <d.smolag@gmail.com>
2018-03-14 14:26:10 +0100

    added menu

--- script/main.js
+++ script/main.js
@@ -1,6 +1,21 @@
 window.addEventListener('load', () => {
   $("#loader").animate({
     opacity: 0
-  }, 1000);
-
+  }, 1000,'linear', () => $("#loader").hide())
 });
+
+(() => {
+    $(document).ready(() => {
+        $(".top_menu").hide();
+        $(() => {
+            $(window).scroll(() => {
+                if ($(this).scrollTop() > 200) {
+                    $('.top_menu').fadeIn();
+                } else {
+                    $('.top_menu').fadeOut();
+                }
+            });
+        });
+
+    });
+})();
