8add8e65a67477064d8e26c40341786b817ecff1
Darqoo <d.smolag@gmail.com>
2018-03-20 12:36:21 +0100

    #2 modyfied scroll one page function

--- script/main.js
+++ script/main.js
@@ -5,59 +5,72 @@ window.addEventListener('load', () => {
 });
 
 (() => {
-    $(document).ready(() => {
+  $(document).ready(() => {
 
-        var position = 0;
+    var position = 0;
 
-        var a = $('.nav-item a')
-        c = [];
-        for (let i = 0; i < a.length; i++) {
-          c.push(a[i].hash);
-        }
+    var a = $('.nav-item a')
+    c = [];
+    for (let i = 0; i < a.length; i++) {
+      c.push(a[i].hash);
+    }
 
-        var sCroll = (e) => {
-          document.removeEventListener('wheel', sCroll)
-          e.preventDefault();
+    var sCroll = (e) => {
+      console.log(window.scrollY + window.innerHeight);
+      console.log($(c[position]).offset().top + $(c[position]).innerHeight());
+      document.removeEventListener('wheel', sCroll);
+      (tryScollPosition = () => {
+        if (window.scrollY + window.innerHeight < $(c[position]).offset().top + $(c[position]).innerHeight()) {
+          $('body').css('overflow', '');
+          console.log('scroluje')
+          document.addEventListener('wheel', sCroll);
+        } else {
+          $('body').css('overflow', 'hidden');
+          console.log('przeskok');
           if (e.deltaY > 0 && position !== c.length - 1) {
             position++
           } else if (e.deltaY < 0 && position !== 0) {
             position--
           }
-          console.log(position)
+          console.log(position);
           $('html, body').animate({
             scrollTop: window.innerHeight >= 400 ? $(c[position]).offset().top - 61 : $(c[position]).offset().top
           }, 800, function() {
             window.location.hash = c[position];
             window.scrollTo(0, window.innerHeight >= 400 ? $(c[position]).offset().top - 61 : $(c[position]).offset().top);
-            document.addEventListener('wheel', sCroll)
-          });
-
-        }
-$('body').css('overflow', 'hidden');
-        document.addEventListener('wheel', sCroll);
-
-      $(window).scroll((e) => {
-        if ($(window).scrollTop() == 0 || window.scrollY + window.innerHeight === document.body.offsetHeight) {
-          $('.bottom-menu').fadeIn();
-        } else {
-          $('.bottom-menu').fadeOut();
-        }
-
-
-
-      }); $("a").on('click', function(event) {
-        if (this.hash !== "") {
-          event.preventDefault();
-          var hash = this.hash;
-          $('html, body').animate({
-            scrollTop: window.innerHeight >= 400 ? $(hash).offset().top - 61 : $(hash).offset().top
-          }, 800, function() {
-            window.location.hash = hash;
-            window.scrollTo(0, window.innerHeight >= 400 ? $(hash).offset().top - 61 : $(hash).offset().top);
+            document.addEventListener('wheel', sCroll);
+            $('body').css('overflow', '');
           });
         }
-      });
+      })();
+    }
+
+
+    document.addEventListener('wheel', sCroll);
+
+    $(window).scroll((e) => {
+      if ($(window).scrollTop() == 0 || window.scrollY + window.innerHeight === document.body.offsetHeight) {
+        $('.bottom-menu').fadeIn();
+      } else {
+        $('.bottom-menu').fadeOut();
+      }
+
+
+
     });
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: window.innerHeight >= 400 ? $(hash).offset().top - 61 : $(hash).offset().top
+        }, 800, function() {
+          window.location.hash = hash;
+          window.scrollTo(0, window.innerHeight >= 400 ? $(hash).offset().top - 61 : $(hash).offset().top);
+        });
+      }
+    });
+  });
 })();
 
 $(window).click(function() {
